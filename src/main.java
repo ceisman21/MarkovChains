aabc20c71d61c445bdb5c78a38f7412e28903a52
ceisman21 <ceisman21@kentdenver.org>
2021-01-27 21:07:36 -0700

    Final Added

--- src/main.java
+++ src/main.java
@@ -5,18 +5,50 @@ import java.io.FileNotFoundException;
 
 public class main {
     public static void main(String[] args) throws FileNotFoundException {
-        File file = new File("./resources/TrumpSpeech.txt");
+        //Find what the user wants
+        Scanner s = new Scanner(System.in);
+        System.out.println("Welcome to the Markov Chain Generator. Please select a file to train from: ");
+        System.out.println("1) Bee Movie Script");
+        System.out.println("2) Book of Genesis");
+        System.out.println("3) Donald Trump's State of the Union Address");
+        System.out.println("4) Your Own File");
+        System.out.print("Your choice: ");
+        int user = s.nextInt();
+        System.out.println();
+
+        File file = null;
+        if (user == 1) {
+            file = new File("./resources/Bee.txt");
+        } else if (user == 2) {
+            file = new File("./resources/Genesis.txt");
+        } else if(user == 3) {
+            file = new File("./resources/TrumpSpeech.txt");
+        } else if (user == 4) {
+            System.out.println("In order to use your own file, please ensure it has been placed in the Resources Directory.");
+            System.out.print("File Name: ");
+            String fileName = s.next();
+            file = new File("./resources/" + fileName);
+            System.out.println();
+        } else {
+            System.out.println("Not a Valid Expression");
+            System.exit(69);
+        }
+
+
+
         Scanner fileScan = new Scanner(file);
+
+        //Scans
         fileScan.useDelimiter(" ");
 
         KVArrays kva = new KVArrays();
 
-        //iterate
+        //iterate through text file
         String firstWord = "";
         String secondWord = "";
         String temp = "";
         while(fileScan.hasNext()) {
-            temp = fileScan.next();
+            temp = fileScan.next().toLowerCase();
             if((temp.endsWith("."))) {
                 secondWord = (temp.substring(0,temp.length()-1));
                 kva.add(firstWord, secondWord);
@@ -45,8 +77,8 @@ public class main {
                 kva.add(firstWord, temp);
             }
             firstWord = temp;
-        }
 
+        }
         System.out.println(kva.getWords(100));
     }
 }
